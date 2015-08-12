3f9486933a204616713f38b1dade4218018f2e92
root <317085872@qq.com>
2015-08-12 03:14:05 +0000

    lua wrapper, not completed

---
+++ common/luaengine/LuaEngine.h
@@ -0,0 +1,58 @@
+/**
+FileName : LuaEngine.h
+Author   : rick <rick.han@yahoo.com>
+
+Copyright (c) <2015> <rick>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+**/
+#ifndef __LUAENGINE__H__
+#define __LUAENGINE__H__
+#include "lua.hpp"
+#include <boost/noncopyable.hpp>
+
+class LuaEngine : public boost::noncopyable
+{
+public:
+  static LuaEngine& instance()
+  {
+    static LuaEngine luaEngine;
+    return luaEngine;
+  }
+
+  lua_State* state() { return _L; }
+
+  // 加载lua代码文件
+  void loadFile(const char* fileName);
+
+  // 注册c/c++库
+  void registLib(const char* libname, const luaL_Reg* lib);
+private:
+  LuaEngine() : _L(luaL_newstate())
+  {
+    luaL_openlibs(_L);
+  }
+
+private:
+  lua_State*    _L;
+};
+
+
+
+#endif // __LUAENGINE__H__
