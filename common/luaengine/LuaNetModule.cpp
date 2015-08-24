2a340b8824bd39344ad50913690201712af2066a
root <317085872@qq.com>
2015-08-24 03:23:02 +0000

    tmp commit

--- common/luaengine/LuaNetModule.cpp
+++ common/luaengine/LuaNetModule.cpp
@@ -35,6 +35,10 @@ struct luaTcpClient
   TcpClient* _client;
 };
 
+void handleConnected(const ErrorCode& err, const TcpConnPtr& conn)
+{
+}
+
 static int newTcpClient(lua_State* L)
 {
   int args = lua_gettop(L);
@@ -43,15 +47,42 @@ static int newTcpClient(lua_State* L)
     return 0;
   }
 
-  //const char* ip = lua_tostring(L, 1);
-  //int port = lua_tointeger(L, 2);
+  const char* ip = lua_tostring(L, 1);
+  int port = lua_tointeger(L, 2);
+  std::string strIp = "127.0.0.1";
+  if (ip != nullptr) {
+    strIp = ip;
+  }
 
   luaTcpClient* client = (luaTcpClient*)lua_newuserdata(L, sizeof(luaTcpClient));
   client->_client = NULL;
   luaL_getmetatable(L, TCPCLIENT_META);
   lua_setmetatable(L, -2);
+  NetworkManager& manager = NetworkManager::instance();
+  client->_client = new TcpClient(manager.getIoService(), strIp, port,
+                                  boost::bind(handleConnected, _1, _2));
 
   return 1;
+}
+
+static int disconnect(lua_State* L)
+{
+  luaTcpClient* client = (luaTcpClient*)luaL_checkudata(L, 1, TCPCLIENT_META);
+  if (client != nullptr && client->_client != nullptr) {
+    client->_client->disconnect();
+  }
+
+  return 0;
+}
+
+static int connect(lua_State* L)
+{
+  luaTcpClient* client = (luaTcpClient*)luaL_checkudata(L, 1, TCPCLIENT_META);
+  if (client != nullptr && client->_client != nullptr) {
+    client->_client->connect();
+  }
+
+  return 0;
 }
 
 void openTcpClient(lua_State* L) 
@@ -60,6 +91,10 @@ void openTcpClient(lua_State* L)
 
   // method table
   lua_newtable(L);
+  lua_pushcfunction(L, disconnect);
+  lua_setfield(L, -2, "disconnect");
+  lua_pushcfunction(L, connect);
+  lua_setfield(L, -2, "connect");
 }
 
 const static luaL_Reg net_funcs[] = 
